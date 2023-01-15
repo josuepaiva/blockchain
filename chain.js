aa6efe34986120583fd229130762be7b9b9e822b
Josue Paiva <josuepaiva@lavid.ufpb.br>
2023-01-15 16:32:33 -0300

    feat: adds function resposible by create next block

--- chain.js
+++ chain.js
@@ -1,6 +1,7 @@
 let Block = require('./block').Block;
 let BlockHeader = require('./block').BlockHeader;
 let moment = require('moment');
+const CryptoJS = require("crypto-js");
 
 let getGenesisBlock = () => {
     let blockHeader = new BlockHeader(1, null, '0x1bc3300000000000000000000000000000000000000000000', moment().unix());
@@ -25,6 +26,19 @@ let getBlock = (index) => {
     }
 };
 
+const generateNextBlock = (txns) => {
+    const prevBlock = getLatestBlock(),
+        prevMerkleRoot = prevBlock.blockHeader.merkleRoot;
+        nextIndex = prevBlock.index + 1,
+        nextTime = moment().unix(),
+        nextMerkleRoot = CryptoJS.SHA256(1, prevMerkleRoot, nextTime).toString();
+
+    const blockHeader = new BlockHeader(1, prevMerkleRoot, nextMerkleRoot, nextTime);
+    const newBlock = new Block(blockHeader, nextIndex, txns);
+    blockchain.push(newBlock);
+    return newBlock;
+};
+
 const blockchain = [getGenesisBlock()];
 
 if (typeof exports != 'undefined') {
@@ -32,4 +46,5 @@ if (typeof exports != 'undefined') {
     exports.getBlock = getBlock;
     exports.blockchain = blockchain;
     exports.getLatestBlock = getLatestBlock;
+    exports.generateNextBlock = generateNextBlock;
 }
